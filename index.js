3229a936996333143b8857b67595fc2eb99baf09
Marcelus <marcelus20felipe@gmail.com>
2021-06-13 00:46:30 +0100

    removed index.js from git track

--- index.js
+++
@@ -1,8 +0,0 @@
-const { default: readFile } = require("./lib/utils/readFile")
-const { default: validateNotNull } = require("./lib/utils/validations/validateNotNull")
-const { default: validateString } = require("./lib/utils/validations/validateString")
-
-
-module.exports = validateString
-module.exports = validateNotNull
-module.exports = readFile
