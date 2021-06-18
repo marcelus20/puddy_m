d75feac478f40921d4cb7dd5f9cb9b1a88c5cb86
marcelus20 <marcelus20felipe@gmail.com>
2021-06-18 20:59:23 +0100

    moved the index.js inside the src folder and added a prepublish script to package.json to build/compile and copy the package.json under ./lib

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import fileFunctions from "./fileFunctions";
+import utils from "./utils";
+import validators from "./validations";
+
+const puddy_m = {
+  utils,
+  fileFunctions,
+  validators,
+};
